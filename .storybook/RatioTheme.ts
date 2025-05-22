951c8e0915e98393090be0e7055937d81d27a79e
Neesh <avaneeshsamsi@gmail.com>
2025-05-22 17:57:58 +0200

    feat: Color adjustments

--- .storybook/RatioTheme.ts
+++ .storybook/RatioTheme.ts
@@ -15,8 +15,8 @@ export default create({
   // colorSecondary: "#ff0000", // Sidebar selected color
 
   // UI
-  appBg: "#0A0A0A",
-  appContentBg: "#262626",
+  appBg: "#171717",
+  appContentBg: "#0A0A0A",
   // appPreviewBg: "#ff0000",
   appBorderColor: "#525252",
   appBorderRadius: 4,
