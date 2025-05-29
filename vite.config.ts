dae38aacf02e2b2c84f5343a4253b94059287e0e
Neesh <avaneeshsamsi@gmail.com>
2025-05-29 13:55:48 +0200

    feat: Configure project as package

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,32 @@
+import { dirname, resolve } from "node:path"
+import { fileURLToPath } from "node:url"
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
 
-// https://vite.dev/config/
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "ratio-ui",
+      // the proper extensions will be added
+      fileName: "ratio-ui",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["react", "react-dom"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: "React",
+          "react-dom/client": "ReactDOM",
+        },
+      },
+    },
+  },
 })
