dd196381546c72c64d57665a45f254f80b3d10cc
Neesh <avaneeshsamsi@gmail.com>
2025-05-30 17:13:58 +0200

    feat: Use margin types

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,22 +1,76 @@
 import type { ComponentPropsWithoutRef, ElementType, ReactElement } from "react"
+import type { SpacingKey } from "./types"
+import { Tokens } from "./types"
 import { cn } from "../lib/utils"
 
-// type TextAs = "span" | "p" | "label" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6"
+const {
+  Margin,
+  MarginLeft,
+  MarginTop,
+  MarginRight,
+  MarginBottom,
+  MarginX,
+  MarginY,
+} = Tokens
+
+export type TextElement =
+  | "span"
+  | "p"
+  | "label"
+  | "h1"
+  | "h2"
+  | "h3"
+  | "h4"
+  | "h5"
+  | "h6"
+
+type MarginProps = {
+  margin?: SpacingKey
+  marginLeft?: SpacingKey
+  marginTop?: SpacingKey
+  marginRight?: SpacingKey
+  marginBottom?: SpacingKey
+  marginX?: SpacingKey
+  marginY?: SpacingKey
+}
 
 type TextProps<T extends ElementType> = {
-  as?: T
-} & Omit<ComponentPropsWithoutRef<T>, "style">
+  as?: T extends TextElement ? T : never
+} & MarginProps &
+  Omit<ComponentPropsWithoutRef<T>, "style">
 
 export default function Text<T extends ElementType = "span">({
   as,
   children,
+  margin,
+  marginLeft,
+  marginTop,
+  marginRight,
+  marginBottom,
+  marginX,
+  marginY,
   className,
   ...restProps
 }: TextProps<T>): ReactElement {
   const Tag = as || "span"
 
   return (
-    <Tag className={cn("", className)} {...restProps}>
+    <Tag
+      className={cn(
+        "",
+        Tokens.Background["surface-1"],
+        Tokens.Text.foreground,
+        margin && Margin[margin],
+        marginX && MarginX[marginX],
+        marginY && MarginY[marginY],
+        marginLeft && MarginLeft[marginLeft],
+        marginTop && MarginTop[marginTop],
+        marginRight && MarginRight[marginRight],
+        marginBottom && MarginBottom[marginBottom],
+        className,
+      )}
+      {...restProps}
+    >
       {children}
     </Tag>
   )
