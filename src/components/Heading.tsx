d5a914fcfb98251b1e89822bc99e34a888903121
Neesh <avaneeshsamsi@gmail.com>
2025-05-31 17:11:10 +0200

    feat: Heading component

---
+++ src/components/Heading.tsx
@@ -0,0 +1,54 @@
+import type { HTMLAttributes, PropsWithChildren } from "react"
+import { cn } from "../lib/utils"
+import { cva } from "cva"
+import Text, { type TextElement } from "./Text"
+import { Tokens, type SpacingKey } from "./types"
+
+type HeadingElement = Extract<
+  TextElement,
+  "h1" | "h2" | "h3" | "h4" | "h5" | "h6"
+>
+
+type HeadingProps = PropsWithChildren & {
+  level: HeadingElement
+  displayLevel: HeadingElement
+  marginBottom?: SpacingKey
+  className?: string
+} & Omit<HTMLAttributes<HTMLHeadElement>, "style">
+
+const heading = cva({
+  base: "",
+  variants: {
+    level: {
+      h1: `text-size-110 leading-none font-medium ${Tokens.MarginBottom[10]}`,
+      h2: `text-size-100 leading-none font-medium${Tokens.MarginBottom[8]}`,
+      h3: `text-size-90 leading-[1.1] font-medium${Tokens.MarginBottom[6]}`,
+      h4: `text-size-80 leading-tight font-medium${Tokens.MarginBottom[4]}`,
+      h5: `text-size-70 leading-tight font-medium${Tokens.MarginBottom[4]}`,
+      h6: `text-size-60 leading-snug font-medium ${Tokens.MarginBottom[4]}`,
+    },
+  },
+})
+
+export default function Heading({
+  level = "h1",
+  displayLevel,
+  marginBottom,
+  children,
+  className,
+  ...restProps
+}: HeadingProps) {
+  return (
+    <Text
+      as={level}
+      className={cn(
+        heading({ level: displayLevel }),
+        marginBottom && Tokens.MarginBottom[marginBottom],
+        className,
+      )}
+      {...restProps}
+    >
+      {children}
+    </Text>
+  )
+}
