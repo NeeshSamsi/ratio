2234df4d00ccaf840c3475e3c2602b69c1d4cf5d
Neesh <avaneeshsamsi@gmail.com>
2025-06-02 13:33:57 +0200

    feat: Basic list component

---
+++ src/components/List.tsx
@@ -0,0 +1,73 @@
+import type { ComponentPropsWithoutRef, ReactElement } from "react"
+import type { SpacingKey } from "../tokens"
+import Box from "./Box"
+
+type ListElement = "ul" | "ol"
+
+type ListProps = ComponentPropsWithoutRef<ListElement> & {
+  type: ListElement
+  styleType: OrderedListVariant | UnorderedListVariant
+  children: ReactElement<typeof ListItem> | ReactElement<typeof ListItem>[]
+  marginBottom?: SpacingKey
+  indent?: SpacingKey
+}
+
+type OrderedListVariant = "decimal" | "lower-alpha" | "lower-roman"
+
+type UnorderedListVariant = "disc" | "square" | "check"
+
+const ListStyleType = {
+  decimal: "list-decimal",
+  "lower-alpha": "list-[lower-alpha]",
+  "lower-roman": "list-[lower-roman]",
+  disc: "list-disc",
+  square: "list-[square]",
+  check: "list-['✓_']",
+}
+
+function List({
+  type,
+  indent = "10",
+  marginBottom = "4",
+  styleType,
+  children,
+  ...restProps
+}: ListProps) {
+  return (
+    <Box
+      as={type}
+      {...restProps}
+      marginBottom={marginBottom}
+      marginLeft={indent}
+      className={ListStyleType[styleType]}
+    >
+      {children}
+    </Box>
+  )
+}
+
+type OrderedListProps = Omit<ListProps, "type" | "styleType"> & {
+  variant?: OrderedListVariant
+}
+
+export function OrderedList({
+  variant = "decimal",
+  ...props
+}: OrderedListProps) {
+  return <List type="ol" styleType={variant} {...props} />
+}
+
+type UnorderedListProps = Omit<ListProps, "type" | "styleType"> & {
+  variant?: UnorderedListVariant
+}
+
+export function UnorderedList({
+  variant = "disc",
+  ...props
+}: UnorderedListProps) {
+  return <List type="ul" styleType={variant} {...props} />
+}
+
+export function ListItem({ children }: { children: React.ReactNode }) {
+  return <Box as="li">{children}</Box>
+}
