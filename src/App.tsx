7256d8b5bdcbaec5c56ed3c8b6d1df58bf884aca
Neesh <avaneeshsamsi@gmail.com>
2025-06-02 10:43:07 +0200

    feat: Test components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import Box from "./components/Box"
+import Heading from "./components/Heading"
+import Paragraph from "./components/Paragraph"
+import ThemeProvider from "./components/ThemeProvider"
+
 function App() {
-  return <h1>Ratio UI</h1>
+  return (
+    <ThemeProvider>
+      <Box as="main">
+        <Heading level="h1" displayLevel="h1">
+          Ratio UI
+        </Heading>
+        <Paragraph size="base">iosuhdf ouihsdfi h</Paragraph>
+      </Box>
+    </ThemeProvider>
+  )
 }
 
 export default App
