79d4dccc195c79c3448271eef9a21b9a182602eb
Neesh <avaneeshsamsi@gmail.com>
2025-06-03 11:31:11 +0200

    feat: More testing components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import Anchor from "./components/Anchor"
 import Box from "./components/Box"
+import Callout from "./components/Callout"
 import Heading from "./components/Heading"
+import HelpText from "./components/HelpText"
+import { ListItem, UnorderedList } from "./components/List/"
 import Paragraph from "./components/Paragraph"
+import Stack from "./components/Stack/stack"
 import ThemeProvider from "./components/ThemeProvider"
 
 function App() {
@@ -10,7 +15,30 @@ function App() {
         <Heading level="h1" displayLevel="h1">
           Ratio UI
         </Heading>
-        <Paragraph size="base">iosuhdf ouihsdfi h</Paragraph>
+
+        <Stack orientation="horizontal" gap="6">
+          <Paragraph size="base">iosuhdf ouihsdfi h</Paragraph>
+          <Paragraph size="base">Something else</Paragraph>
+        </Stack>
+
+        <UnorderedList variant="disc">
+          <ListItem>Testing</ListItem>
+          <ListItem>Testing</ListItem>
+          <ListItem>Testing</ListItem>
+        </UnorderedList>
+
+        <Box padding="8" display="flex" flexDirection="col" gap="4">
+          <Callout variant="success" title="Read me, I'm a callout"></Callout>
+
+          <HelpText variant="info">Testing help text</HelpText>
+
+          <Anchor
+            size="lg"
+            label="Explore"
+            link="https://vsco.co"
+            icon="arrow-right"
+          />
+        </Box>
       </Box>
     </ThemeProvider>
   )
