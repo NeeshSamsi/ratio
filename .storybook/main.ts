34a8cc8026ac8769f2ec1e3fa78d1f8757549150
Neesh <avaneeshsamsi@gmail.com>
2025-06-05 16:00:28 +0200

    feat: Manually order pages

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,7 @@
 import type { StorybookConfig } from "@storybook/react-vite"
 
 const config: StorybookConfig = {
-  stories: [
-    "../src/stories/main/welcome.mdx",
-    "../src/stories/main/vsco.mdx",
-    "../src/stories/main/foundations.mdx",
-    "../src/stories/main/voice-and-tone.mdx",
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
