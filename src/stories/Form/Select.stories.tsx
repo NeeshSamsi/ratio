e5e3236576bf90a77f0771ceaee73a330a524041
Neesh <avaneeshsamsi@gmail.com>
2025-06-05 17:15:08 +0200

    feat: Select and story

---
+++ src/stories/Form/Select.stories.tsx
@@ -0,0 +1,63 @@
+import type { ComponentProps } from "react"
+import type { Meta, StoryObj } from "@storybook/react-vite"
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+type StoryProps = ComponentProps<typeof Select>
+
+export default {
+  component: Select,
+  title: "Form/Select",
+  argTypes: {
+    disabled: {
+      control: "boolean",
+    },
+    defaultValue: {
+      control: "text",
+    },
+  },
+  tags: ["autodocs"],
+} satisfies Meta<StoryProps>
+
+type Story = StoryObj<StoryProps>
+
+export const Default: Story = {
+  render: (props) => (
+    <Select {...props}>
+      <SelectTrigger>
+        <SelectValue placeholder="Select an option" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectItem value="apple">Apple</SelectItem>
+          <SelectItem value="banana">Banana</SelectItem>
+          <SelectItem value="orange">Orange</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  ),
+  args: {
+    // defaultValue: "apple",
+  },
+}
+
+export const Disabled: Story = {
+  render: (props) => (
+    <Select {...props}>
+      <SelectTrigger>
+        <SelectValue placeholder="Select an option" />
+      </SelectTrigger>
+    </Select>
+  ),
+  args: {
+    disabled: true,
+  },
+}
